2f3850bdbd3d45984833d943b9b43c909ebd1b74
esthoril <shendrickx@hotmail.com>
2016-04-17 14:23:08 +0200

    update launcher

--- MCE3/launcher.sh
+++ MCE3/launcher.sh
@@ -2,14 +2,24 @@
 # launcher.sh
 # navigate to home dir, then to this dir, execute the python script, then back home
 
+LOGFILE=/home/pi/hackathon/restart.log
+
+writelog() {
+  now='date'
+  echo ="$now $*" >> $LOGFILE
+}
+
 
 sleep 1  # pause so the os can init the pins
 
+writelog "Starting"
 whilte true; do
   cd /
   cd home/pi
   cd hackathon
   sudo python PiHouse.py
   #sudo python PiHouseLED.py
+  writelog "Exited with status $?"
+  writelog "Restarting"
 done
 cd /
